b971ddd92ca940be275712c0fe770642c01cc36c
Carsten Wulff <carstenw@ntnu.no>
2021-07-21 20:33:13 +0200

    Updated to systemverilog

--- project/verilog/pixelSensor.v
+++ project/verilog/pixelSensor.v
@@ -1,55 +1,106 @@
+//====================================================================
+//        Copyright (c) 2021 Carsten Wulff Software, Norway
+// ===================================================================
+// Created       : wulff at 2021-7-21
+// ===================================================================
+//  The MIT License (MIT)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//====================================================================
 
-module pixelSensor(anaBias1, anaBias2, anaRamp, anaReset, pixErase, pixTx, pixData, pixRead);
+//----------------------------------------------------------------
+// Model of pixel sensor, including
+//  - Reset
+//  - The sensor
+//  - Comparator
+//  - Memory latch
+//  - Readout of latched value
+//----------------------------------------------------------------
+module PIXEL_SENSOR
+  (
+   input logic      VBN1,
+   input logic      VBN2,
+   input logic      RAMP,
+   input logic      RESET,
+   input logic      ERASE,
+   input logic      EXPOSE,
+   input logic      READ,
+   inout wire [7:0] DATA
 
-   parameter real v_erase = 1.2;
-   parameter real lsb = v_erase/255;
-   parameter real dv_pixel = 0.5;
+   );
 
+   real             v_erase = 1.2;
+   real             lsb = v_erase/255;
+   parameter real   dv_pixel = 0.5;
 
-   input wire        anaBias1, anaBias2, anaRamp, anaReset;
-   input wire        pixErase, pixTx, pixRead;
-   inout wire [7:0]  pixData;
+   real             tmp;
+   logic            cmp;
+   real             adc;
 
-   real              tmp;
-   reg              cmp;
-   real              adc;
+   logic [7:0]      p_data;
 
-   reg[7:0]       p_data;
-
-   initial begin
-      tmp = 0;
-      cmp =0;
-   end
-
+   //----------------------------------------------------------------
+   // RESET
+   //----------------------------------------------------------------
    // Reset the pixel value on pixRst
-   always @(pixErase) begin
+   always @(ERASE) begin
       tmp = v_erase;
       p_data = 0;
       cmp  = 0;
+      adc = 0;
    end
 
-   // Use anaBias1 to provide a clock for integration when pixTx is high
-   always @(posedge anaBias1) begin
-      if(pixTx)
+   //----------------------------------------------------------------
+   // SENSOR
+   //----------------------------------------------------------------
+   // Use bias to provide a clock for integration when exposing
+   always @(posedge VBN1) begin
+      if(EXPOSE)
         tmp = tmp - dv_pixel*lsb;
    end
 
-   // Use anaRamp to provide a clock for ADC conversion, assume that anaRamp
-   // and pixData is synchronous
-   always @(posedge anaRamp) begin
+   //----------------------------------------------------------------
+   // Comparator
+   //----------------------------------------------------------------
+   // Use ramp to provide a clock for ADC conversion, assume that ramp
+   // and DATA are synchronous
+   always @(posedge RAMP) begin
       adc = adc + lsb;
       if(adc > tmp)
         cmp <= 1;
    end
 
-   always @(posedge cmp) begin
-      if(cmp) begin
-         p_data <= pixData;
+   //----------------------------------------------------------------
+   // Memory latch
+   //----------------------------------------------------------------
+   always_comb  begin
+      if(!cmp) begin
+         p_data = DATA;
       end
 
    end
 
+   //----------------------------------------------------------------
+   // Readout
+   //----------------------------------------------------------------
    // Assign data to bus when pixRead = 0
-   assign pixData = pixRead ? p_data : 8'bZ;
+   assign DATA = READ ? p_data : 8'bZ;
 
 endmodule // re_control
--- project/verilog/pixelSensor_tb.v
+++ project/verilog/pixelSensor_tb.v
@@ -1,15 +1,50 @@
+//====================================================================
+//        Copyright (c) 2021 Carsten Wulff Software, Norway
+// ===================================================================
+// Created       : wulff at 2021-7-21
+// ===================================================================
+//  The MIT License (MIT)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//====================================================================
 
 
+`timescale 1 ns / 1 ps
 
+//====================================================================
+// Testbench for pixelSensor
+// - clock
+// - instanciate pixel
+// - State Machine for controlling pixel sensor
+// - Model the ADC and ADC
+// - Readout of the databus
+// - Stuff neded for testbench. Store the output file etc.
+//====================================================================
 module pixelSensor_tb;
-
 
    //------------------------------------------------------------
    // Testbench clock
    //------------------------------------------------------------
-   reg clk =0;
-   reg reset =0;
-   parameter integer clk_period = 1;
+   logic clk =0;
+   logic reset =0;
+   parameter integer clk_period = 1000;
    parameter integer sim_end = clk_period*2400;
    always #clk_period clk=~clk;
 
@@ -19,36 +54,32 @@ module pixelSensor_tb;
    parameter real    dv_pixel = 0.5;  //Set the expected photodiode current (0-1)
 
    //Analog signals
-   wire              anaBias1;
-   wire              anaBias2;
-   wire              anaRamp;
-   wire              anaReset;
+   logic              anaBias1;
+   logic              anaBias2;
+   logic              anaRamp;
+   logic              anaReset;
 
    //Tie off the unused lines
    assign anaBias2 = 1;
    assign anaReset = 1;
 
    //Digital
-   wire              pixErase;
-   wire              pixTx;
-   wire [7:0]        pixData;
-   wire              pixRead;
+   logic              erase;
+   logic              expose;
+   logic              read;
+   wire [7:0]         pixData; //  We need this to be a wire, because we're tristating it
 
    //Instanciate the pixel
-   pixelSensor  #(.dv_pixel(dv_pixel))  ps1(anaBias1, anaBias2, anaRamp, anaReset, pixErase, pixTx, pixData,pixRead);
-
+   PIXEL_SENSOR  #(.dv_pixel(dv_pixel))  ps1(anaBias1, anaBias2, anaRamp, anaReset, erase,expose, read,pixData);
 
    //------------------------------------------------------------
    // State Machine
    //------------------------------------------------------------
    parameter ERASE=0, EXPOSE=1, CONVERT=2, READ=3, IDLE=4;
-   reg               erase;
-   reg               expose;
-   reg               read;
-   reg               convert;
-   reg               convert_stop;
 
-   reg [2:0]         state,next_state;   //States
+   logic               convert;
+   logic               convert_stop;
+   logic [2:0]         state,next_state;   //States
    integer           counter;            //Delay counter in state machine
 
    //State duration in clock cycles
@@ -57,13 +88,8 @@ module pixelSensor_tb;
    parameter integer c_convert = 5;
    parameter integer c_read = 5;
 
-   //Connect to the pixel
-   assign pixRst = erase;
-   assign pixTx = expose;
-   assign pixRead = read;
-
    // Control the output signals
-   always @(state) begin
+   always_ff @(negedge clk ) begin
       case(state)
         ERASE: begin
            erase <= 1;
@@ -94,7 +120,7 @@ module pixelSensor_tb;
    end // always @ (state)
 
    // Control the state transitions
-   always @(posedge clk or posedge reset) begin
+   always_ff @(posedge clk or posedge reset) begin
       if(reset) begin
          state = IDLE;
          next_state = ERASE;
@@ -141,8 +167,7 @@ module pixelSensor_tb;
    //------------------------------------------------------------
    // DAC and ADC model
    //------------------------------------------------------------
-
-   reg[7:0] data;
+   logic[7:0] data;
    integer  val;
 
    // If we are to convert, then provide a clock via anaRamp
@@ -152,14 +177,14 @@ module pixelSensor_tb;
 
    // During expoure, provide a clock via anaBias1.
    // Again, no resemblence to real world, but we cheat.
-   assign anaBias1 = pixTx ? clk : 0;
+   assign anaBias1 = expose ? clk : 0;
 
    // If we're not reading the pixData, then we should drive the bus
-   assign pixData = pixRead ? 8'bZ: data;
+   assign pixData = read ? 8'bZ: data;
 
    // When convert, then run a analog ramp (via anaRamp clock) and digtal ramp via
    // data bus. Assert convert_stop to return control to main state machine.
-   always @(posedge clk or reset) begin
+   always_ff @(posedge clk or posedge reset) begin
       if(reset) begin
          convert_stop =0;
          data =0;
@@ -178,10 +203,10 @@ module pixelSensor_tb;
    end // always @ (posedge clk or reset)
 
    //------------------------------------------------------------
-   // Readout from pixelbus
+   // Readout from databus
    //------------------------------------------------------------
-   reg [7:0] pixelDataOut;
-   always @(posedge clk or reset) begin
+   logic [7:0] pixelDataOut;
+   always_ff @(posedge clk or posedge reset) begin
       if(reset) begin
          pixelDataOut = 0;
       end
@@ -205,7 +230,7 @@ module pixelSensor_tb;
 
         #sim_end
           $stop;
-        #sim_end $finish;
+
 
      end
 
