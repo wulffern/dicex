eb69a098d8ffe32fa8598f348c44ce18f0946812
Carsten Wulff <carsten@wulff.no>
2021-08-09 22:28:34 +0200

    added mazeplot

--- maze/plotmaze.py
+++ maze/plotmaze.py
@@ -3,7 +3,8 @@
 import pygame
 import sys
 import re
-
+import os
+import subprocess
 pygame.init()
 
 size = width, height = 800, 800
@@ -58,6 +59,8 @@ with open("turtle.txt","r") as fi:
 
 (x,y) = turtle[0]
 
+filenamelist = [0]*len(turtle)
+os.makedirs("pics", exist_ok=True)
 i = 0
 while 1:
     for event in pygame.event.get():
@@ -67,7 +70,24 @@ while 1:
         pygame.draw.rect(screen, redvisited,pygame.Rect(x,y,step+1,step+1))
         (x,y) = turtle[i]
         pygame.draw.rect(screen, red,pygame.Rect(x,y,step+1,step+1))
-        i+=1
 
-    pygame.display.flip()
-    clock.tick(40)
+        filenamelist[i] = "pics/pic" + str(i) + ".png"
+        pygame.image.save(screen, filenamelist[i])
+        pygame.display.flip()
+        i+=1
+    else:
+        #Combine into a GIF using ImageMagick's "convert"-command (called using subprocess.call()):
+        convertexepath = "convert"  # Hardcoded
+        convertcommand = [convertexepath, "-delay", "1", "-size", str(width) + "x" + str(height)] + filenamelist + ["maze.gif"]
+        print(convertcommand)
+        subprocess.call(convertcommand)
+
+        #Remove the PNG files (if they were meant to be temporary):
+        for filename in filenamelist:
+            os.remove(filename)
+
+
+
+    #Save as PNG images on disk:
+
+    #clock.tick(100)
