1a567e0991227bccc29708a616eb4097658f911b
Carsten Wulff <carsten@wulff.no>
2021-10-06 23:06:48 +0200

    Updated counter_sv

--- sim/verilog/counter_sv/counter.v
+++ sim/verilog/counter_sv/counter.v
@@ -1,16 +1,21 @@
 module counter(
                output logic [WIDTH-1:0] out,
-               input logic             clk,
-               input logic             reset
+               input logic              clk,
+               input logic              reset
                );
 
    parameter WIDTH = 8;
+
+   logic [WIDTH-1:0]                    count;
+   always_comb begin
+      count = out + 1;
+   end
 
    always_ff @(posedge clk or posedge reset) begin
       if (reset)
         out <= 0;
       else
-        out <= out + 1;
+        out <= count;
    end
 
 endmodule // counter
