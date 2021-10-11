b4b6505d9ed700e85f07397c4b80635e3f940069
Carsten Wulff <carsten@wulff.no>
2021-10-11 09:24:49 +0200

    Separated out fsm

--- project/verilog/pixelSensorFsm.v
+++ project/verilog/pixelSensorFsm.v
@@ -1,7 +1,7 @@
 //====================================================================
 //        Copyright (c) 2021 Carsten Wulff Software, Norway
 // ===================================================================
-// Created       : wulff at 2021-7-21
+// Created       : wulff at 2021-10-11
 // ===================================================================
 //  The MIT License (MIT)
 //
@@ -28,63 +28,34 @@
 
 `timescale 1 ns / 1 ps
 
-//====================================================================
-// Testbench for pixelSensor
-// - clock
-// - instanciate pixel
-// - State Machine for controlling pixel sensor
-// - Model the ADC and ADC
-// - Readout of the databus
-// - Stuff neded for testbench. Store the output file etc.
-//====================================================================
-module pixelSensor_tb;
 
-   //------------------------------------------------------------
-   // Testbench clock
-   //------------------------------------------------------------
-   logic clk =0;
-   logic reset =0;
-   parameter integer clk_period = 500;
-   parameter integer sim_end = clk_period*2400;
-   always #clk_period clk=~clk;
+module pixelSensorFsm(
+                       input logic        clk,
+                       input  logic      reset,
+                       output logic erase,
+                       output logic expose,
+                       output logic read,
+                      output logic convert
 
-   //------------------------------------------------------------
-   // Pixel
-   //------------------------------------------------------------
-   parameter real    dv_pixel = 0.5;  //Set the expected photodiode current (0-1)
+                      );
 
-   //Analog signals
-   logic              anaBias1;
-   logic              anaRamp;
-   logic              anaReset;
 
-   //Tie off the unused lines
-   assign anaReset = 1;
-
-   //Digital
-   logic              erase;
-   logic              expose;
-   logic              read;
-   tri[7:0]         pixData; //  We need this to be a wire, because we're tristating it
-
-   //Instanciate the pixel
-   PIXEL_SENSOR  #(.dv_pixel(dv_pixel))  ps1(anaBias1, anaRamp, anaReset, erase,expose, read,pixData);
+     //State duration in clock cycles
+   parameter integer c_erase = 5;
+   parameter integer c_expose = 255;
+   parameter integer c_convert = 255;
+   parameter integer c_read = 5;
 
    //------------------------------------------------------------
    // State Machine
    //------------------------------------------------------------
    parameter ERASE=0, EXPOSE=1, CONVERT=2, READ=3, IDLE=4;
 
-   logic               convert;
+
    logic               convert_stop;
    logic [2:0]         state,next_state;   //States
-   integer           counter;            //Delay counter in state machine
+   integer        counter;
 
-   //State duration in clock cycles
-   parameter integer c_erase = 5;
-   parameter integer c_expose = 255;
-   parameter integer c_convert = 255;
-   parameter integer c_read = 5;
 
    // Control the output signals
    always_ff @(negedge clk ) begin
@@ -169,67 +140,5 @@ module pixelSensor_tb;
            counter = counter + 1;
       end
    end // always @ (posedge clk or posedge reset)
-
-   //------------------------------------------------------------
-   // DAC and ADC model
-   //------------------------------------------------------------
-   logic[7:0] data;
-
-   // If we are to convert, then provide a clock via anaRamp
-   // This does not model the real world behavior, as anaRamp would be a voltage from the ADC
-   // however, we cheat
-   assign anaRamp = convert ? clk : 0;
-
-   // During expoure, provide a clock via anaBias1.
-   // Again, no resemblence to real world, but we cheat.
-   assign anaBias1 = expose ? clk : 0;
-
-   // If we're not reading the pixData, then we should drive the bus
-   assign pixData = read ? 8'bZ: data;
-
-   // When convert, then run a analog ramp (via anaRamp clock) and digtal ramp via
-   // data bus.
-   always_ff @(posedge clk or posedge reset) begin
-      if(reset) begin
-         data =0;
-      end
-      if(convert) begin
-         data +=  1;
-      end
-      else begin
-         data = 0;
-      end
-   end // always @ (posedge clk or reset)
-
-   //------------------------------------------------------------
-   // Readout from databus
-   //------------------------------------------------------------
-   logic [7:0] pixelDataOut;
-   always_ff @(posedge clk or posedge reset) begin
-      if(reset) begin
-         pixelDataOut = 0;
-      end
-      else begin
-         if(read)
-           pixelDataOut <= pixData;
-      end
-   end
-
-   //------------------------------------------------------------
-   // Testbench stuff
-   //------------------------------------------------------------
-   initial
-     begin
-        reset = 1;
-
-        #clk_period  reset=0;
-
-        $dumpfile("pixelSensor_tb.vcd");
-        $dumpvars(0,pixelSensor_tb);
-
-        #sim_end
-          $stop;
-
-     end
 
 endmodule // test
--- project/verilog/pixelSensorFsm_tb.v
+++ project/verilog/pixelSensorFsm_tb.v
@@ -62,113 +62,19 @@ module pixelSensor_tb;
    assign anaReset = 1;
 
    //Digital
-   logic              erase;
-   logic              expose;
-   logic              read;
+   wire              erase;
+   wire              expose;
+   wire             read;
+   wire             convert;
+
    tri[7:0]         pixData; //  We need this to be a wire, because we're tristating it
 
    //Instanciate the pixel
    PIXEL_SENSOR  #(.dv_pixel(dv_pixel))  ps1(anaBias1, anaRamp, anaReset, erase,expose, read,pixData);
 
-   //------------------------------------------------------------
-   // State Machine
-   //------------------------------------------------------------
-   parameter ERASE=0, EXPOSE=1, CONVERT=2, READ=3, IDLE=4;
+   pixelSensorFsm #(.c_erase(5),.c_expose(255),.c_convert(255),.c_read(5))
+   fsm1(.clk(clk),.reset(reset),.erase(erase),.expose(expose),.read(read),.convert(convert));
 
-   logic               convert;
-   logic               convert_stop;
-   logic [2:0]         state,next_state;   //States
-   integer           counter;            //Delay counter in state machine
-
-   //State duration in clock cycles
-   parameter integer c_erase = 5;
-   parameter integer c_expose = 255;
-   parameter integer c_convert = 255;
-   parameter integer c_read = 5;
-
-   // Control the output signals
-   always_ff @(negedge clk ) begin
-      case(state)
-        ERASE: begin
-           erase <= 1;
-           read <= 0;
-           expose <= 0;
-           convert <= 0;
-        end
-        EXPOSE: begin
-           erase <= 0;
-           read <= 0;
-           expose <= 1;
-           convert <= 0;
-        end
-        CONVERT: begin
-           erase <= 0;
-           read <= 0;
-           expose <= 0;
-           convert = 1;
-        end
-        READ: begin
-           erase <= 0;
-           read <= 1;
-           expose <= 0;
-           convert <= 0;
-        end
-        IDLE: begin
-           erase <= 0;
-           read <= 0;
-           expose <= 0;
-           convert <= 0;
-
-        end
-      endcase // case (state)
-   end // always @ (state)
-
-
-   // Control the state transitions.
-   //TODO: The counter should probably be an always_comb. Might be a good idea
-   //to also reset the counter from the state machine, i.e provide the count
-   //down value, and trigger on 0
-   always_ff @(posedge clk or posedge reset) begin
-      if(reset) begin
-         state = IDLE;
-         next_state = ERASE;
-         counter  = 0;
-         convert  = 0;
-      end
-      else begin
-         case (state)
-           ERASE: begin
-              if(counter == c_erase) begin
-                 next_state <= EXPOSE;
-                 state <= IDLE;
-              end
-           end
-           EXPOSE: begin
-              if(counter == c_expose) begin
-                 next_state <= CONVERT;
-                 state <= IDLE;
-              end
-           end
-           CONVERT: begin
-              if(counter == c_convert) begin
-                 next_state <= READ;
-                 state <= IDLE;
-              end
-           end
-           READ:
-             if(counter == c_read) begin
-                state <= IDLE;
-                next_state <= ERASE;
-             end
-           IDLE:
-             state <= next_state;
-         endcase // case (state)
-         if(state == IDLE)
-           counter = 0;
-         else
-           counter = counter + 1;
-      end
-   end // always @ (posedge clk or posedge reset)
 
    //------------------------------------------------------------
    // DAC and ADC model
