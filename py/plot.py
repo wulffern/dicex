2e7295352fdfbc253267714e5fcdebc507f2c43f
Carsten Wulff <carsten@wulff.no>
2021-10-18 18:21:38 +0200

    Updated plot

--- py/plot.py
+++ py/plot.py
@@ -8,10 +8,7 @@ import math
 import tikzplotlib
 
 
-def plot(xname,yname,ptype=None):
-
-    if("same" not in ptype):
-        f = plt.figure()
+def plot(xname,yname,ptype=None,ax=None):
 
     df = pd.read_csv(fname,header=4 )
     x = df[xname]
@@ -21,27 +18,27 @@ def plot(xname,yname,ptype=None):
     #- Plot
 
     if("logy" in ptype):
-        plt.semilogy(x,y,label=yname)
+        ax.semilogy(x,y,label=yname)
     elif("ln2" in ptype):
-        plt.plot(x,np.log(y)/np.log(2),label=yname)
+        ax.plot(x,np.log(y)/np.log(2),label=yname)
     elif("logx" in ptype):
-        plt.semilogx(x,y,label=yname)
+        ax.semilogx(x,y,label=yname)
     elif("db20" in ptype):
-        plt.semilogx(x,20*np.log10(y),label="dB20(" + yname + ")")
+        ax.semilogx(x,20*np.log10(y),label="dB20(" + yname + ")")
     else:
-        plt.plot(x,y,label=yname)
+        ax.plot(x,y,label=yname)
 
-    plt.legend()
-    plt.xlabel(xname)
-    if(ptype == None):
-        plt.ylabel(yname)
-    plt.grid()
+    ax.legend()
+
+    if(ptype == ""):
+        ax.set_ylabel(yname)
+    ax.grid()
 
 
 if(len(sys.argv) < 4):
     print("Usage python3 plot.py <file.csv> <x-name> <y-name,[y-name]> ")
 
-ptype = None
+ptype = ""
 if(len(sys.argv) > 4):
     ptype = sys.argv[4]
 
@@ -50,14 +47,31 @@ xname = sys.argv[2]
 yname = sys.argv[3]
 
 
-
 if("," in yname):
     names = yname.split(",")
-    for n in names:
-        plot(xname,n,ptype)
-else:
-    plot(xname,yname,ptype)
 
-tikzplotlib.save(fname.replace(".csv",".pgf"))
+
+    if("same" in ptype):
+        f,axes = plt.subplots(1,1)
+    else:
+        f,axes = plt.subplots(len(names),1,sharex=True)
+
+    for i in range(0,len(names)):
+        if("same" in ptype):
+            plot(xname,names[i],ptype,ax=axes)
+        else:
+            plot(xname,names[i],ptype,ax=axes[i])
+    plt.xlabel(xname + "(" + fname + ")")
+else:
+    f,axes = plt.subplots(1,1)
+    plot(xname,yname,ptype,axes)
+
+plt.tight_layout()
+
+if("tikz" in ptype):
+    tikzplotlib.save(fname.replace(".csv",".pgf"))
+
 plt.savefig(fname.replace(".csv",".pdf"))
-plt.show()
+
+if("pdf" not in ptype):
+    plt.show()
